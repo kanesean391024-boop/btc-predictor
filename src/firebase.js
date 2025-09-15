3666c046c157a4ca3fcf400a1da2b7bb3eae1cf6
kanesean391024-boop <kanesean391024@gmail.com>
2025-09-15 10:59:02 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+import { getAnalytics } from "firebase/firestore";
 
 const firebaseConfig = {
   firebaseConfig = {
@@ -13,4 +14,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);

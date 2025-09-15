fd2a56db9e10ff8061cdd59a18c70c7c192e8762
kanesean391024-boop <kanesean391024@gmail.com>
2025-09-14 21:09:21 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  firebaseConfig = {
+  apiKey: "AIzaSyA4tVBL8uT1TG6Ro6jxxXTEyk7i35thDd4",
+  authDomain: "btc-predictor-6a9d9.firebaseapp.com",
+  projectId: "btc-predictor-6a9d9",
+  storageBucket: "btc-predictor-6a9d9.firebasestorage.app",
+  messagingSenderId: "261649645889",
+  appId: "1:261649645889:web:a24d3b38675fd924fd21c0",
+  measurementId: "G-892EBKT0ZJ"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
